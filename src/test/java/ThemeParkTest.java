017406a6c1482658a0328cd479235dac80c019cf
Richard <fahitch+github@gmail.com>
2020-01-14 16:23:07 +0000

    add canAddInterests to ThemeParkTest

--- src/test/java/ThemeParkTest.java
+++ src/test/java/ThemeParkTest.java
@@ -1,9 +1,7 @@
-import attractions.Dodgems;
-import attractions.Park;
-import attractions.Playground;
-import attractions.RollerCoaster;
+import attractions.*;
 import org.junit.Before;
 import org.junit.Test;
+import stalls.*;
 
 import javax.management.relation.Role;
 
@@ -15,6 +13,10 @@ public class ThemeParkTest {
     Park park;
     Playground playground;
     RollerCoaster rollerCoaster;
+    CandyflossStall candyFlossStall;
+    IceCreamStall iceCreamStall;
+    TobaccoStall tobaccoStall;
+    ParkingSpot parkingSpot1;
 
     @Before
     public void before(){
@@ -23,13 +25,28 @@ public class ThemeParkTest {
         park = new Park("The Magic Park", 3);
         playground = new Playground("The Magic Playground", 10);
         rollerCoaster = new RollerCoaster("The Magic Rollercoaster", 20);
+        parkingSpot1 = new ParkingSpot(ParkingSpot.A1);
+        candyFlossStall = new CandyflossStall("The Magic Candy Floss Stall","Magic Mike", parkingSpot1);
+        tobaccoStall = new TobaccoStall("The Magic Tobacco Stall","Magic Tyke", parkingSpot1);
+        iceCreamStall = new IceCreamStall("The Magic Ice Cream Stall", "Magic Ryke", parkingSpot1);
     }
 
     @Test
     public void startsEmpty(){
         assertEquals(0,themePark.getAttractions().size());
         assertEquals(0,themePark.getStalls().size());
-
     }
 
+    @Test
+    public void canAddItems(){
+        themePark.addToAttractions(dodgem);
+        themePark.addToAttractions(park);
+        themePark.addToAttractions(playground);
+        themePark.addToAttractions(rollerCoaster);
+        themePark.addToStalls(candyFlossStall);
+        themePark.addToStalls(tobaccoStall);
+        themePark.addToStalls(iceCreamStall);
+        assertEquals(4,themePark.getAttractions().size());
+        assertEquals(3,themePark.getStalls().size());
+    }
 }
